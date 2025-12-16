a6bd15ccee3fad24de30931a1249c51e203de6bb
조선대-NHN 29 <chosun-nhn29@joseondae-NHNui-noteubug-2.local>
2025-12-16 10:44:21 +0900

    Initial commit

---
+++ src/main/java/com/nhnacademy/bookssearchworker/BooksSearchWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookssearchworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksSearchWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksSearchWorkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/bookssearchworker/BooksSearchWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookssearchworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksSearchWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
